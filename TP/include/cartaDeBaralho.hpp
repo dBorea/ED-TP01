1da6d38cae7ec5bb63f53cb9322b8179836b575f
Daniel Alves <danielresende2000@gmail.com>
2022-05-23 02:01:49 -0300

    Pequenas alterações na classe CartaDeBaralho

--- TP/include/cartaDeBaralho.hpp
+++ TP/include/cartaDeBaralho.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include "msgassert.hpp"
 
 using std::string;
 using std::stoi;
@@ -20,16 +21,16 @@ class CartaDeBaralho {
 			numero = stoi(input.substr(0, input.size() - 1));		// Divide o input entre 'número' e 'naipe' usando substr
 			naipe = input.substr(input.size() - 1);
 		}
-		string getNaipe() { return naipe; }
-		int getNum() { return numero; }
-		void setNaipe(string input) { naipe = input; }
-		void setNum(int num) { numero = num; }
-		int dimensao() { return numero; }
-		void printCarta() {
+		string getNaipe() const { return naipe; }
+		int getNum() const { return numero; }
+		int dimensao() const { return numero; }				// Talvez não use
+		void printCarta() const {								// Talvez não use
 			std::cout << std::endl << "Carta inserida:" << std::endl
 				 << "\t Naipe: " << getNaipe() << std::endl
 				 << "\t Numero: " << getNum() << std::endl;
 		}
+		void setNaipe(string input) { naipe = input; }
+		void setNum(int num) { numero = num; }
 		void setCarta(string input){ *this = CartaDeBaralho(input); }
 };
 
